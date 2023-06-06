4892b889bfd673c684be7762c8a57c96c4c4ff47
ehp2021 <emmahpaik@gmail.com>
2023-06-06 16:40:52 -0500

    add first ver

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # fundscraper
+
+Just a side project I built to pull in news around "Series A" funding from Google News and save down certain pieces of information to FireStore
+
+I'm still looking for the right serverless computing setup to automate this process
---
+++ scraper.py
@@ -0,0 +1,32 @@
+import requests
+from bs4 import BeautifulSoup
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+cred = credentials.Certificate("./serviceAccountKey.json")
+firebase_admin.initialize_app(cred)
+db = firestore.client()
+
+search_keywords = "Series A"
+url = f"https://news.google.com/search?q={search_keywords}&hl=en-US&gl=US&ceid=US%3Aen"
+response = requests.get(url)
+
+soup = BeautifulSoup(response.content, "html.parser")
+
+articles = soup.find_all("article")
+for article in articles:
+    title = article.find("h3").text
+    link = article.find("a")["href"]
+    description = article.find("p").text
+
+    # Extract other relevant information from the article as needed
+
+    # Save the extracted data to Firebase
+    doc_ref = db.collection("articles").document()
+    doc_ref.set({
+        "title": title,
+        "link": link,
+        "description": description,
+        # Add other relevant fields to the document
+    })
